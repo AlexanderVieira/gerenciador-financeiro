1a8414a7cb039bb604b57c385edfe4a5564be63d
Alexander <Alexander@DESKTOP-8RM83T5>
2019-04-09 18:28:48 -0300

    Implementando filtro por data e quantidade de lançamentos.

--- app/components/home.component.js
+++ app/components/home.component.js
@@ -3,12 +3,18 @@
 
     angular.module('gfpApp').component('home', {
         controllerAs: 'vm',
-        controller: function (noticiaService) {
+        controller: function (noticiaService, $sessionStorage) {
 
             var vm = this;
             vm.noticias = [];
             vm.noticia = {};
             vm.ocultar = true;
+
+            let sessionIsLogged = $sessionStorage.isLogged;
+            if (sessionIsLogged == null) {
+                vm.isLogged = $sessionStorage.isLogged = false;
+            }
+            vm.isLogged = $sessionStorage.isLogged;
 
             vm.$onInit = function () {
                 noticiaService.getAll().then(function (response) {
